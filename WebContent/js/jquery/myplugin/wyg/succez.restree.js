5492f58cf4deb19f9c6f523db552b20f05097bf7
wangyg <498812492@qq.com>
2011-02-28 17:53:51 +0800

    研究jquery对xml的一些操作

--- WebContent/js/jquery/myplugin/wyg/succez.restree.js
+++ WebContent/js/jquery/myplugin/wyg/succez.restree.js
@@ -1,4 +1,24 @@
 (function($){
+/*
+ * 根据传入的XML路径去获取xml，现在用一个自定义函数，以后可以改用jquery的ajax
+ * 函数，因为ajax函数在本地无法装入xml文件，只能在服务器上运行，但是ajax函数的
+ * 执行效率却是下边该函数的几倍,另外该方法也有跨域请求的问题，待解决，在fixfoc
+ * 中无法运行
+ */	
+	function loadTreeXML(xmlpath){
+	    var xmlDoc=null;
+	    if(window.ActiveXObject){
+	        xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
+	    }
+	    else if(document.implementation && document.implementation.createDocument){
+	        xmlDoc=document.implementation.createDocument("","",null);
+	    }else{
+	        alert("this has an error happened!");
+	    }
+	    xmlDoc.async=false;
+	    xmlDoc.load(xmlpath);
+	    return xmlDoc;
+	};	
 	/*
 	* 处理节点的展开和收起事件
 	*/	
@@ -51,7 +71,21 @@
     		$(this).css("cursor","default");
 			moveFlag=false;
     	});
-	}
+	};
+/*
+ * 根据传入的xml路径去初始化一颗资源树
+ */	
+	$.fn.initTreeByXml=function(xmlpath){
+		var xmlDoc=loadTreeXML(xmlpath);
+		if (xmlDoc){		
+		$(xmlDoc).find("nodes > rootnode").each(function(){
+			var temp=$(this).find(">leafnode").text();
+			alert(temp);
+			var temp=$(this).find(">rootnode").attr("text");
+			alert(temp);
+		});
+		}; 	
+	};
 })(jQuery);
 
  
