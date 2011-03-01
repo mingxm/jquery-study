29734c73573634fca849f74b7b7957ee0c0c9078
xh <xtyyumi301@gmail.com>
2011-03-01 16:27:04 +0800

    提交这2天的学习内容

---
+++ WebContent/js/jquery/myplugin/xh/errorcontent.js
@@ -0,0 +1,9 @@
+(function($){
+	if(typeof(debug3)=="function"){
+		debug3("Hi, I'm errorcontent.js!There are some error here.");
+		debug3("\tdo.");
+		debug3("\tdo.");
+		debug3("\terror occue.");
+		_errorcall_();
+	}
+})(jQuery);
--- src/com/jquery/xh/AjaxTestAction.java
+++ src/com/jquery/xh/AjaxTestAction.java
@@ -1,42 +1,52 @@
 package com.jquery.xh;
 
-import java.io.File;
 import java.io.IOException;
 
-import javax.servlet.Servlet;
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.dom4j.Document;
-import org.dom4j.DocumentFactory;
-import org.dom4j.Element;
 
-public class AjaxTestAction implements Servlet{
+public class AjaxTestAction extends HttpServlet{
 
-	public void destroy() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public ServletConfig getServletConfig() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public String getServletInfo() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public void init(ServletConfig arg0) throws ServletException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void service(ServletRequest req, ServletResponse resp)
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+		String action = req.getParameter("action");
+		if("getparam".equalsIgnoreCase(action)){
+			getParam(req, resp);
+		}else if("getcodes".equalsIgnoreCase(action)){
+			getCode(resp);
+		}else{
+			throw new RuntimeException("不支持 action--"+action);
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		doGet(req, resp);
+	}
+
+	private void getParam(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		String name = req.getParameter("name");
+		String value;
+		if("USERNAME".equalsIgnoreCase(name)){
+			value = System.getenv("username");
+		}else if("java.version".equalsIgnoreCase(name)){
+			value = System.getProperty("java.version");
+		}else if(req.getParameterMap().containsKey(name)){
+			value = req.getParameter(name);
+		}else{
+			throw new RuntimeException("不支持参数--"+name);
+		}
+		resp.getOutputStream().print(value);
+	}
+
+	private void getCode(HttpServletResponse resp) throws IOException {
 		String dir = "D:/esendev/study/jquery-study/WebContent/"; //html文件所在的路径。
 		
 		Document doc = XhFn.getFileStruct(dir);
